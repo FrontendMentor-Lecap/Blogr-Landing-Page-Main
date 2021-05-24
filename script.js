f02dace10728a3b8e0ffda972dc8393a82c1d112
Luis Bernal <cap.luisb@gmail.com>
2021-05-24 10:45:39 -0500

    Js burger modification

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const burger = document.getElementById('burger-icon');
-const xIcon = document.getElementById('cross-icon');
 const hdrNav = document.querySelector('.header__navigation');
 const catNav = document.querySelectorAll('.main__category');
 const subNav = document.querySelectorAll('.sub__navigation');
 
 burger.addEventListener('click', () => {
   hdrNav.classList.toggle('active');
-  xIcon.style.display = 'unset';
-  burger.style.display = 'none';
-});
-
-xIcon.addEventListener('click', () => {
-  hdrNav.classList.toggle('active');
-  xIcon.style.display = 'none';
-  burger.style.display = 'unset';
+  if (hdrNav.classList.contains('active')) {
+    burger.src = './images/icon-close.svg';
+  } else {
+    burger.src = './images/icon-hamburger.svg';
+  }
 });
