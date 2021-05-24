65c5c6ae4c78f8c5ee3e28d1f5dc70e9e71c2535
Luis Bernal <cap.luisb@gmail.com>
2021-05-24 18:02:11 -0500

    mobile complete

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const burger = document.getElementById('burger-icon');
 const hdrNav = document.querySelector('.header__navigation');
-const catNav = document.querySelectorAll('.main__category');
-const subNav = document.querySelectorAll('.sub__navigation');
 
 burger.addEventListener('click', () => {
   hdrNav.classList.toggle('active');
