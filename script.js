9a174af9ab18630a5f30ec0234aeb261ebdc25ae
Luis Bernal <cap.luisb@gmail.com>
2021-05-23 21:21:31 -0500

    Mobile menu advances

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const burger = document.getElementById('burger-icon');
+const xIcon = document.getElementById('cross-icon');
 const hdrNav = document.querySelector('.header__navigation');
+const catNav = document.querySelectorAll('.main__category');
+const subNav = document.querySelectorAll('.sub__navigation');
 
 burger.addEventListener('click', () => {
-  hdrNav.classList.toggle('mobile');
+  hdrNav.classList.toggle('active');
+  xIcon.style.display = 'unset';
+  burger.style.display = 'none';
+});
+
+xIcon.addEventListener('click', () => {
+  hdrNav.classList.toggle('active');
+  xIcon.style.display = 'none';
+  burger.style.display = 'unset';
 });
