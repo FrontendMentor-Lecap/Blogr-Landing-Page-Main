ad31a5cc51e57c1b8f8036b4954a95fca0235901
Luis Bernal <cap.luisb@gmail.com>
2021-05-25 18:15:01 -0500

    Sticky burger button fix

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ burger.addEventListener('click', () => {
   hdrNav.classList.toggle('active');
   if (hdrNav.classList.contains('active')) {
     burger.src = './images/icon-close.svg';
+    burger.style.position = 'fixed';
   } else {
     burger.src = './images/icon-hamburger.svg';
+    burger.style.position = 'unset';
   }
 });
